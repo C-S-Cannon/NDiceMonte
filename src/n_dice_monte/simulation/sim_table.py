d7f2fb76234309367b4fcfabf25764589e4db3dc
Sphinx <null>
2026-01-02 22:35:47 -0800

    added simulation module

---
+++ src/n_dice_monte/simulation/sim_table.py
@@ -0,0 +1,22 @@
+"""
+a collection of functions to allow the creation of data sets for dice
+"""
+
+import random
+import numpy as np
+
+from .dice import Dice
+
+
+def sim_max(dice_size, pool_size, sim_size):
+    """
+    create an array holding of the max value rolled for a pool of dice across some arbitrary size
+    """
+
+    dice = Dice(dice_size)
+    sim_arr = np.array(sim_size)
+
+    for x in range(sim_size):
+        sim_arr[x] = dice.roll_max(pool_size)
+
+    return sim_arr
