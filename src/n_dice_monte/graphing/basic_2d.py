be9f916ef82b703d5adf785d4f483ad847c1df42
Sphinx <null>
2026-01-03 13:57:17 -0800

    corrections to graphing method

--- src/n_dice_monte/graphing/basic_2d.py
+++ src/n_dice_monte/graphing/basic_2d.py
@@ -3,19 +3,29 @@ basic graphing functions for x-y representations
 """
 
 import matplotlib.pyplot as plt
-#import numpy as np
+from matplotlib import ticker
+import numpy as np
 
-def histogram( sim_data, dice_size, min_face=1, style='stepfilled',
+def histogram( sim_data, max_val, min_val=1, style='stepfilled',
     colors=('xkcd:cerulean',), stack=False, name=("",)):
     """
     creation of a 2d histogram for visualizing dice results
 
-    sim_data is input data, dice_size is the number of dice faces,
-    min_face is the starting face of the dice, sim_width is how many dice per simulation pool,
+    sim_data is input data, max_val is the maximum value a group of data can be,
+    min_val is the minimum value, sim_width is how many dice per simulation pool,
     colors are xkcd color names, stack and style are graph settings
     """
 
+    _fig, ax = plt.subplots()
+
     #generate a single histogram
-    ax = plt.hist(sim_data, dice_size, [min_face, dice_size],
-        density=True, histtype=style, color=colors, stacked=stack, label=name)
+    ax.hist(sim_data, bins=np.arange(min_val,max_val+2, dtype=int)-0.5,
+        range=[min_val, max_val], density=True, histtype=style, color=colors,
+        stacked=stack, label=name)
+
+    ax.set_xlabel('Roll Value')
+    ax.set_ylabel('Chance')
+    ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1))
+
+
     return ax
