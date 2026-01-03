f235e1d89c7ede0debbe12728f3044533c177fa6
Sphinx <null>
2026-01-03 15:44:01 -0800

    corrected ticks

--- dev/six_20s.py
+++ dev/six_20s.py
@@ -8,23 +8,21 @@ import numpy as np
 from n_dice_monte import sim_max
 from n_dice_monte import histogram
 
-SIM_SIZE = 500
+SIM_SIZE = 10000
 DICE_FACE = 20
 DICE_POOL = 6
 
 sim_arr = sim_max(SIM_SIZE, DICE_FACE, DICE_POOL)
 avg = np.mean(sim_arr)
 
-ax = plt.subplots()
-ax[0].set_title('6d20 Maxpool Distribution')
-ax[0].legend(title="Mean is {:.2f}".format(avg), loc='upper left', reverse=True)
+fig, ax = plt.subplots()
 
 ax = histogram(sim_arr, DICE_FACE)
+ax.set_title('6d20 Maxpool Distribution')
+ax.legend(title="Mean is {:.2f}".format(avg), loc='upper left', reverse=True)
 
 
 #ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
 
-plt.savefig("./dev/six_20s.jpg")
-bins = np.arange(1,DICE_FACE+2, dtype=int)-0.5
-print("array bins are")
-print(bins)
+#plt.savefig("./dev/six_20s.jpg")
+plt.show()
--- src/n_dice_monte/graphing/basic_2d.py
+++ src/n_dice_monte/graphing/basic_2d.py
@@ -6,6 +6,40 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
 
+def major_ticks(max_val):
+    """
+    accepts the maximum value of a histogram and returns an array of appropriate major ticks
+    """
+
+    tick_count = 0
+    if max_val<=6:
+        tick_count = max_val
+    elif max_val<=20:
+        tick_count = max_val/2
+    elif max_val <=50:
+        tick_count = max_val/5
+    else:
+        tick_count = 10
+
+    tick_count = int(tick_count)
+    ticks = np.zeros(tick_count)
+
+    for x in range(tick_count):
+        ticks[x] = int((x+1) * max_val / tick_count)
+
+    return ticks
+
+def minor_ticks(max_val):
+    """
+    accepts the maximum value of a histogram and returns an array of appropriate minor ticks
+    """
+
+    ticks = np.zeros(max_val)
+    for x in range(max_val):
+        ticks[x] = x+1
+
+    return ticks
+
 def histogram( sim_data, max_val, min_val=1, style='stepfilled',
     colors=('xkcd:cerulean',), stack=False, name=("",)):
     """
@@ -19,13 +53,24 @@ def histogram( sim_data, max_val, min_val=1, style='stepfilled',
     _fig, ax = plt.subplots()
 
     #generate a single histogram
-    ax.hist(sim_data, bins=np.arange(min_val,max_val+2, dtype=int)-0.5,
+    _n, _bins, _patches = ax.hist(sim_data, bins=np.arange(min_val,max_val+2, dtype=int)-0.5,
         range=[min_val, max_val], density=True, histtype=style, color=colors,
         stacked=stack, label=name)
 
+
     ax.set_xlabel('Roll Value')
     ax.set_ylabel('Chance')
+
+    #convert y axis to display as a percentage chance rather than norm from 1
     ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1))
 
+    #previous method of setting x ticks
+    #ax.xaxis.set_major_locator(ticker.MaxNLocator(
+    #        nbins=max_val, integer=True, steps=[1, 2, 4, 5, 10]))
+    #ax.xaxis.set_minor_locator(ticker.MultipleLocator(base=1))
+
+    #set x axis tick locations and values
+    ax.set_xticks(major_ticks(max_val))
+    ax.set_xticks(minor_ticks(max_val), minor=True)
 
     return ax
