1a2e45f1fdeebc46ad9b85566a5b1d3d168184f6
Sphinx <null>
2026-01-04 21:46:20 -0800

    added new file for distribution tools

---
+++ src/n_dice_monte/data_tools/distributions.py
@@ -0,0 +1,27 @@
+"""
+some functions to calculate distributions such as means or quartiles
+"""
+
+import numpy as np
+
+def am_array(sim_data):
+    """
+    create a 1d array of means from a 2d array of simulations
+    each simulation occupies an individual row of the data array
+    """
+
+    #determine how many simulation rows there are and construct array
+    sim_count = np.shape(sim_data)[0]
+
+    #construct array to be used for storing arithmatic means
+    means = np.array(sim_count)
+
+    for x in range(sim_count):
+        means[x]=np.mean(sim_data[x])
+
+    return means
+
+def qrt_low(sim_data):
+    """
+    return an array of lower bound quartiles of a set of data
+    """
