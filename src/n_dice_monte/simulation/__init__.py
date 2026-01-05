9319af8f4e03569d67b1c240eaf746675017dbc9
Sphinx <null>
2026-01-04 23:04:07 -0800

    IntEnum for dice sizes

--- src/n_dice_monte/simulation/__init__.py
+++ src/n_dice_monte/simulation/__init__.py
@@ -2,12 +2,6 @@
 
 from .dice import Dice
 from .sim_table import sim_max
-
-D4_SIZE = 4
-D6_SIZE = 6
-D8_SIZE = 8
-D10_SIZE = 10
-D12_SIZE = 12
-D20_SIZE = 20
+from .dice_size import DSize
 
 _AUTHOR_ = 'Sphinx'
---
+++ src/n_dice_monte/simulation/dice_size.py
@@ -0,0 +1,16 @@
+"""
+a basic dice size enum to be referenced with any related 
+"""
+
+from enum import IntEnum
+
+class DSize(IntEnum):
+    """
+    IntEnum of dice sizes
+    """
+    D4 = 4
+    D6 = 6
+    D8 = 8
+    D10 = 10
+    D12 = 12
+    D20 = 20
