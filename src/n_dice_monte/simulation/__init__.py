f83cc603b9bbeb7c5aa656368b3fdebca3e8e02f
Sphinx <null>
2026-01-17 06:05:25 -0800

    added sim_pool function to simulations available

--- src/n_dice_monte/simulation/__init__.py
+++ src/n_dice_monte/simulation/__init__.py
@@ -1,7 +1,7 @@
 """module for creating dice monte carlos and deriving useful insights"""
 
 from .dice import Dice
-from .sim_table import sim_max
+from .sim_table import sim_max, sim_pool
 from .dice_size import DSize
 
 _AUTHOR_ = 'Sphinx'
